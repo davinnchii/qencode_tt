b9b10cde8a6fb1b1385c285fc3d325838eb9fa13
davinnchii <zaikoivan99@gmail.com>
2024-03-18 22:43:13 +0100

    Fix SSO button triggering form submit

--- src/components/SSOAuth/ButtonSSO/ButtonSSO.tsx
+++ src/components/SSOAuth/ButtonSSO/ButtonSSO.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const ButtonSSO: React.FC<Props> = ({ buttonText, icon }) => {
   return (
     <div className="w-[50%] px-[20px] py-[14px] rounded-[6px] border-[1px] border-[#D3D8DC] flex justify-center items-center">
-      <button>
+      <button type="button">
       <span className="font-medium text-sm">
         <Image width={18} height={18} src={icon} alt="icon" className="inline mr-2" />
         {buttonText}
